cb7444044b514d837618969e5d0d4b1d34c718c1
Francisco Martin Rico <fmrico@gmail.com>
2020-02-13 11:56:58 +0100

    navigation
    Signed-off-by: Francisco Martin Rico <fmrico@gmail.com>

---
+++ navigation/src/navigate_to_wp.cpp
@@ -0,0 +1,102 @@
+/*********************************************************************
+*  Software License Agreement (BSD License)
+*
+*   Copyright (c) 2018, Intelligent Robotics
+*   All rights reserved.
+*
+*   Redistribution and use in source and binary forms, with or without
+*   modification, are permitted provided that the following conditions
+*   are met:
+
+*    * Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*    * Redistributions in binary form must reproduce the above
+*      copyright notice, this list of conditions and the following
+*      disclaimer in the documentation and/or other materials provided
+*      with the distribution.
+*    * Neither the name of Intelligent Robotics nor the names of its
+*      contributors may be used to endorse or promote products derived
+*      from this software without specific prior written permission.
+
+*   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*   COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*   POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Jonatan Gines jginesclavero@gmail.com */
+
+/* Mantainer: Jonatan Gines jginesclavero@gmail.com */
+#include "actionlib/client/simple_action_client.h"
+#include "move_base_msgs/MoveBaseAction.h"
+#include "geometry_msgs/PoseStamped.h"
+#include <string>
+
+namespace navigation
+{
+class Navigator
+{
+  public:
+    Navigator(ros::NodeHandle& nh) : nh_(nh), action_client_("/move_base", false), goal_sended_(false)
+    {
+      wp_sub_ = nh_.subscribe("/navigate_to", 1, &Navigator::navigateCallback, this);
+    }
+
+    void navigateCallback(geometry_msgs::PoseStamped goal_pose_)
+    {
+      ROS_INFO("[navigate_to_wp] Commanding to (%f %f)", goal_pose_.pose.position.x, goal_pose_.pose.position.y);
+      move_base_msgs::MoveBaseGoal goal;
+      goal.target_pose = goal_pose_;
+      goal.target_pose.header.frame_id = "/map";
+      goal.target_pose.header.stamp = ros::Time::now();
+      action_client_.sendGoal(goal);
+      goal_sended_ = true;
+    }
+
+    void step()
+    {
+      if (goal_sended_)
+      {
+        bool finished_before_timeout = action_client_.waitForResult(ros::Duration(0.5));
+        actionlib::SimpleClientGoalState state = action_client_.getState();
+        if (finished_before_timeout)
+        {
+          actionlib::SimpleClientGoalState state = action_client_.getState();
+          if (state == actionlib::SimpleClientGoalState::SUCCEEDED)
+            ROS_INFO("[navigate_to_wp] Goal Reached!");
+          else
+            ROS_INFO("[navigate_to_wp] Something bad happened!");
+          goal_sended_ = false;
+        }
+      }
+    }
+
+  private:
+    ros::NodeHandle nh_;
+    ros::Subscriber wp_sub_;
+    bool goal_sended_;
+    actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> action_client_;
+
+};
+}  // namespace bica_dialog
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "navigate_to_wp_node");
+  ros::NodeHandle nh("~");
+  navigation::Navigator navigator(nh);
+  while (ros::ok())
+  {
+    navigator.step();
+    ros::spinOnce();
+  }
+  return 0;
+}
